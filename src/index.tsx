9ee86fe7ab5a90cd2d1a88660dbaeded96d7ff9e
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-10-15 14:23:36 -0400

    feat:add more hooks to the index

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,5 @@ export * from './hooks/useField';
 export * from './hooks/useCounter';
 export * from './hooks/useToggle';
 export * from './hooks/useFirebaseFirestore';
+export * from './hooks/useFirebaseStorage';
+export * from './hooks/useFirebaseProfile';
